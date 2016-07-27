7c3a6fa5023a11dce10fd07367c49080aca89160
Simon <saintexupery@snu.ac.kr>
2016-07-28 01:08:28 +0900

    댓글추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from .models import Post
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
 
 def post_list(request):
     post_list = Post.objects.all()
@@ -11,7 +11,20 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    comment_list = Comment.objects.filter(post_id=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = get_object_or_404(Post, pk=pk)
+            comment.save()
+            return redirect('blog:post_detail', pk)
+    else:
+        form = CommentForm()
 
     return render(request, 'blog/post_detail.html', {
         'post' : post,
+        'comment_list' : comment_list,
+        'form' : form,
     })
