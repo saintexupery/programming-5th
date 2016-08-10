1bcc8e4cd6bd58bf132625cc2968b1edaec04e0f
Simon <saintexupery@snu.ac.kr>
2016-08-10 11:42:37 +0900

    auth.form.UserCreationForm을 이용한 회원가입

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
+from . import views
 
 urlpatterns = [
     url(r'^login/$', login, name='login', kwargs={
         'template_name' : 'accounts/login.html',
     }), # kwargs로 넘겨줄 값은 https://github.com/django/django/blob/1.9.8/django/contrib/auth/views.py 에서 확인
+    url(r'^signup/$', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
+from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 
 def signup(request):
     if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL) # 관련 설정은 https://github.com/django/django/blob/1.9.8/django/contrib/auth/views.py 에서 직접 해줄 수 있다. global settings를 받아 온 후 우리의 settings로 overwrite하는 것이기 때문
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
